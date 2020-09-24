e0fd86caba7d5a684e2904776a77d2518211a27c
Jinpyo Joo <68942200+jinpyojoo@users.noreply.github.com>
2020-09-25 07:43:00 +0900

    Update select.php

--- select.php
+++ select.php
@@ -1,9 +1,9 @@
 <?php
 $type = $_GET["type"];
 if ($type == shape) {
-    Header("Location:shape.php");
+    Header("Location:shape.html");
 } else if ($type == name) {
-    Header("Location:name.php");
+    Header("Location:name.html");
 } else {
     echo "비정상적인 접근입니다";
 }
